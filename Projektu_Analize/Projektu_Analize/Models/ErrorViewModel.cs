024b374e4b8e1cc48f8bf62a8346065cfdf70b35
dehoft <edvinas.deksnys@gmail.com>
2019-05-12 14:23:15 +0300

    Sukuriau projekta

---
+++ Projektu_Analize/Projektu_Analize/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projektu_Analize.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
